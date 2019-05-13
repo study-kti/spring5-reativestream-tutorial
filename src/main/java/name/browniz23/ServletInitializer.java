4d34c5b186e59e84ad82f3e978c5c508dd1d2b16
browniz421 <browniz421@gmail.com>
2019-05-13 20:30:12 +0900

    init

---
+++ src/main/java/name/browniz23/ServletInitializer.java
@@ -0,0 +1,13 @@
+package name.browniz23;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring5ReativestreamTutorialApplication.class);
+	}
+
+}
---
+++ src/main/java/name/browniz23/Spring5ReativestreamTutorialApplication.java
@@ -0,0 +1,14 @@
+package name.browniz23;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Spring5ReativestreamTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5ReativestreamTutorialApplication.class, args);
+		System.out.println("Hello reactiveStream");
+	}
+
+}
