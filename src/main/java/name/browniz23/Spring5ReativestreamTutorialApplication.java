03c86ad8d332213a703d39ba6396afde212f9494
browniz421 <browniz421@gmail.com>
2019-05-14 13:08:24 +0900

    hello webflux

--- src/main/java/name/browniz23/Spring5ReativestreamTutorialApplication.java
+++ src/main/java/name/browniz23/Spring5ReativestreamTutorialApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class Spring5ReativestreamTutorialApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5ReativestreamTutorialApplication.class, args);
-		System.out.println("Hello reactiveStream");
+		System.out.println("Hello Spring5 ReactiveStream API");
 	}
 
 }
